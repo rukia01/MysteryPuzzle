220c5837d9ada2260dd1398f6d62ee9162ed00c7
Rukia <syaru223@gmail.com>
2022-06-24 12:46:07 +0900

    new

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Transform playerPos;
-    [SerializeField] GameObject box;
     public Vector3 movePos;
     private Vector3 moveX = new Vector3(1, 0, 0);
     private Vector3 moveY = new Vector3(0, 1, 0);
@@ -13,7 +12,9 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float speed;
     private bool moveJudge = true;
     [SerializeField] LayerMask blockLayer;
-    [SerializeField] private Vector3 previousPos;
+    private int boxMove;
+    RaycastHit2D hit;
+    RaycastHit2D [] hits = new RaycastHit2D [4];
     // Start is called before the first frame update
     void Start()
     {
@@ -23,50 +24,77 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        previousPos = transform.position;
-        Ray2D ray = new Ray2D(transform.position, transform.up);
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up, 1f, blockLayer);
-        if (hit)
+        hits[0] = Physics2D.Raycast(transform.position, transform.up, 1f, blockLayer);
+        hits[1] = Physics2D.Raycast(transform.position, -transform.up, 1f, blockLayer);
+        hits[2] = Physics2D.Raycast(transform.position, transform.right, 1f, blockLayer);
+        hits[3] = Physics2D.Raycast(transform.position, -transform.right, 1f, blockLayer);
+        //Debug.DrawRay(transform.position, transform.up, Color.red);
+        //Debug.DrawRay(transform.position, -transform.up, Color.red);
+        //Debug.DrawRay(transform.position, transform.right, Color.red);
+        //Debug.DrawRay(transform.position, -transform.right, Color.red);
+
+        Debug.Log((hits[0].collider == null) + ":" + (hits[1].collider == null) + ":" + (hits[2].collider == null) + ":" + (hits[3].collider == null)) ;
+        if (hits[0].collider != null || hits[1].collider != null || hits[2].collider != null || hits[3].collider != null)
         {
-            Debug.DrawRay(transform.position, transform.up, Color.red);
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                target = (hit.collider.transform.position - this.transform.position).normalized;
-                hit.collider.transform.Translate(target.x, target.y, 0);
+                hit = hits[boxMove];
+                if (hit.collider != null)
+                {
+                    target = (hit.collider.transform.position - this.transform.position).normalized;
+                    hit.collider.transform.Translate(Mathf.Round(target.x), Mathf.Round(target.y), 0);
+                }
             }
         }
         if (moveJudge)
         {
-            if (Input.GetKeyDown(KeyCode.W)) 
-            { 
-                transform.eulerAngles = Vector3.zero;
-                movePos = transform.position + moveY;
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                boxMove = 0;
+                if (hits[0].collider == null)
+                {
+                    Debug.Log("è„");
+                    movePos = transform.position + moveY;
+                }
                 moveJudge = false;
             }
-            if (Input.GetKeyDown(KeyCode.S)) 
-            { 
-                transform.eulerAngles = new Vector3(0, 0, 180);
-                movePos = transform.position + -moveY;
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                boxMove = 1;
+                if (hits[1].collider == null)
+                {
+                    Debug.Log("â∫");
+                    movePos = transform.position + -moveY;
+                }
                 moveJudge = false;
             }
-            if (Input.GetKeyDown(KeyCode.D)) 
-            { 
-                transform.eulerAngles = new Vector3(0, 0, 270);
-                movePos = transform.position + moveX;
+            else if (Input.GetKeyDown(KeyCode.D))
+            {
+                boxMove = 2;
+                transform.localScale = Vector3.one;
+                if (hits[2].collider == null)
+                {
+                    Debug.Log("âE");
+                    movePos = transform.position + moveX;
+                }
                 moveJudge = false;
             }
-            if (Input.GetKeyDown(KeyCode.A)) 
-            { 
-                transform.eulerAngles = new Vector3(0, 0, 90);
-                movePos = transform.position + -moveX;
+            else if (Input.GetKeyDown(KeyCode.A))
+            {
+                boxMove = 3;
+                transform.localScale = new Vector3(-1,1,1);
+                if (hits[3].collider == null)
+                {
+                    Debug.Log("ç∂");
+                    movePos = transform.position + -moveX;
+                }
                 moveJudge = false;
             }
         }
         playerPos.position = Vector3.MoveTowards(playerPos.position, movePos, speed * Time.deltaTime);
-        if (playerPos.position == movePos) { moveJudge = true; }
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        transform.position = previousPos;
+        if (playerPos.position == movePos)
+        {
+            moveJudge = true;
+        }
     }
 }
